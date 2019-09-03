d3d41441b9bc71b956f3281406f35a5de9bef4c3
unknown <mohamedhani01014@gmail.com>
2019-09-03 02:30:27 +0200

    'send'

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.get('/',(req,res,next)=>{
 });
 app.use(mainPageRoute);
 
-const port = process.env.port || 9000;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-app.listen(config.port,()=>{
+app.listen(server_port,server_host,()=>{
     console.log("connected");
 
 });
