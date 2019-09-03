bb09c74e68605249f19edfc256a1c7b02e504c0a
unknown <mohamedhani01014@gmail.com>
2019-09-03 02:23:48 +0200

    'second'

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const path = require('path');
 const projectPath = require('./utailts/projectPath');
 const mainPageRoute = require('./routes/mainPageRoute');
 const app = express();
-/*
+
 app.set('view engine','ejs');
 app.set('views','views');
 
 app.use(express.static(path.join(projectPath.path,'styles')));
-app.use(bodyparser.urlencoded({extended:false}));*/
+app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json({}));
 app.get('/',(req,res,next)=>{
-    console.log('main page');
+    console.log('hello');
     res.json("hello wold");
 });
-//app.use(mainPageRoute);
+app.use(mainPageRoute);
 
 const port = process.env.port || 9000;
 
