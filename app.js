d54952c1c6f31305c9062ce71042ba895c4dd2e4
unknown <mohamedhani01014@gmail.com>
2019-09-03 02:09:45 +0200

    'he'

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views','views');
 
 app.use(express.static(path.join(projectPath.path,'styles')));
 app.use(bodyparser.urlencoded({extended:false}));
-app.use(bodyparser.json());
+app.use(bodyparser.json({}));
 app.use(mainPageRoute);
 
 const port = process.env.port || 9000;
