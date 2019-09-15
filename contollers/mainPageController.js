a0d36a8a2894270f2784c2a26125af8ef8401395
unknown <mohamedhani01014@gmail.com>
2019-09-15 18:11:23 +0200

    new

--- contollers/mainPageController.js
+++ contollers/mainPageController.js
@@ -9,9 +9,15 @@ exports.getMainPage=(req,res,next)=>{
          if(data!="")
          {
               data  = JSON.parse(data);
-             const myHome = new home(data.temp,data.sw);
+             const myHome = new home(data.temp,data.sw1,data.sw2,data.sw3,data.sw4,data.sw5,data.   sw6);
              console.log(myHome);
-              res.render('mainPage.ejs',{temp:myHome.temp , sw :myHome.sw});
+              res.render('mainPage.ejs',{temp:myHome.temp , 
+                                              sw1 :myHome.sw1,
+                                              sw2:myHome.sw2,
+                                              sw3:myHome.sw3,
+                                              sw4:myHome.sw4,
+                                              sw5:myHome.sw5,
+                                              sw6:myHome.sw6 });
          }
      })
     
@@ -19,9 +25,15 @@ exports.getMainPage=(req,res,next)=>{
 exports.setState=(req,res,next)=>{
      
  var temp = req.body.temp;
- var sw = req.body.sw;
-const myHome  = new home(temp,sw);
-console.log(myHome);
+ var sw1 = req.body.sw1;
+ var sw2 = req.body.sw2;
+ var sw3 = req.body.sw3;
+ var sw4 = req.body.sw4;
+ var sw5 = req.body.sw5;
+ var sw6 = req.body.sw6;
+  
+const myHome  = new home(temp,sw1,sw2,sw3,sw4,sw5,sw6);
+console.log("new state is :"+ myHome);
 fs.writeFile(dataFilePath, JSON.stringify(myHome),()=>{
      res.json('done');
 })
--- models/home.js
+++ models/home.js
@@ -1,9 +1,14 @@
 class home {
 
-   constructor(temp ,sw )
+   constructor(temp ,sw1,sw2,sw3,sw4,sw5,sw6 )
    {
        this.temp = temp;
-       this.sw=sw ;
+       this.sw1=sw1 ;
+       this.sw2=sw2 ;
+       this.sw3=sw3 ;
+       this.sw4=sw4 ;
+       this.sw5=sw5 ;
+       this.sw6=sw6 ;
    }
 
 }
